ca7f9d48f76160d32a13515ae4aa266011eb4229
halimbagusalhakim <halsat132@gmail.com>
2025-11-24 09:18:39 +0700

    inisiasi index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const connectDatabase = require('./config/db');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
